21eda532f936b9726630097506c194434266814f
dj <92108830+DJonGit@users.noreply.github.com>
2021-10-28 17:32:00 +0100

    added photos and some js

---
+++ resources/script/scrips.js
@@ -0,0 +1,30 @@
+
+
+// ------Skills Section --------------
+
+let skillHeadingOver = document.getElementById('skillsHeading');
+let skillsTarget = document.getElementById('skills');
+
+function blackSkills() {
+    skills.style.color = '#000';
+}
+function greySkills() {
+    skills.style.color = '#2f3641';
+}
+
+skillHeadingOver.addEventListener('mouseover', blackSkills);
+skillHeadingOver.addEventListener('mouseout', greySkills);
+
+let skillsOver = document.getElementById('skills');
+
+function color() {
+    skillColor.style.zIndex = '3';
+    // skills.style.color = 'red';
+}
+function bw() {
+    skillColor.style.zIndex = '0';
+}
+
+skillsOver.addEventListener('mouseover', color);
+skillsOver.addEventListener('mouseout', bw);
+
